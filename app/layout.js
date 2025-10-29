6e04e40ac778871f21e4f32eb2459ee9c9b32c3f
VikasKandhal <140228156+VikasKandhal@users.noreply.github.com>
2025-10-29 19:11:31 +0530

    Prep for deployment

---
+++ app/layout.js
@@ -0,0 +1,32 @@
+import { Geist, Geist_Mono, Inter } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/header";
+import { ClerkProvider } from "@clerk/nextjs";
+
+const inter = Inter({subsets:["latin"]});
+
+export const metadata = {
+  title: "Reflct",
+  description: "A journalling app",
+};
+
+export default function RootLayout({ children }) {
+  return (
+     <ClerkProvider>
+    <html lang="en">
+      <body
+        className={`${inter.className}`}
+      >
+      <div className="bg-[url('/bg.jpg')] opacity-50 fixed -z-10 inset-0"/>
+      <Header />
+      <main className="min-h-screen">{children}</main>
+      <footer className="bg-orange-300 py-12 bg-opacity-10">
+        <div className="container mx-auto px-4 text-center text-gray-900">
+          <p>Made By Vikas</p>
+        </div>
+      </footer>
+      </body>
+    </html>
+    </ClerkProvider>
+  );
+}
